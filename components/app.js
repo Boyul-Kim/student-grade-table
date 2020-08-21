ce33f09d1376b8a0c0171cc7e586246f4a1b37b0
Boyul Kim <boyulkim1@gmail.com>
2020-08-20 19:53:07 -0700

    completed requesting data portion

---
+++ components/app.js
@@ -0,0 +1,24 @@
+class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error) {
+    console.error(error);
+  };
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  };
+  getGrades() {
+    $.ajax({
+      type: "GET",
+      headers: { "X-Access-Token": "E2zmM6eF"},
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError,
+    })
+  }
+  start() {
+    this.getGrades();
+  }
+}
---
+++ main.js
@@ -0,0 +1,2 @@
+const test = new App();
+test.start();
