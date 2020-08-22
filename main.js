62f3b32dc5aa6ad7ebfceca0cfb67760639130bb
Boyul Kim <boyulkim1@gmail.com>
2020-08-22 14:41:16 -0700

    completed add a gradeform component portion

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var table = document.querySelector('table');
 var header = document.querySelector('header');
+var form = document.querySelector('form');
+const gradeForm =  new GradeForm(form);
 const pageHeader = new PageHeader(header);
 const gradeTable = new GradeTable(table);
-const appClass = new App(gradeTable, pageHeader);
+const appClass = new App(gradeTable, pageHeader, gradeForm);
 appClass.start();
